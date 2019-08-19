ed14fcd4aa1fd9552fd993bad47d163acac7efd1
Ali Can Kuştemur <alican.kustemur@gmail.com>
2019-08-19 15:47:41 +0300

    add new color

--- darkSlack.sh
+++ darkSlack.sh
@@ -20,7 +20,7 @@ JS="
 ${JS_START}
 document.addEventListener('DOMContentLoaded', function() {
   // Fetch our CSS in parallel ahead of time
-  const cssPath = 'https://raw.githubusercontent.com/alicankustemur/slack-dark-theme/master/slack4';
+  const cssPath = 'https://raw.githubusercontent.com/alicankustemur/slack-dark-theme/master/slack4.css';
   let cssPromise = fetch(cssPath).then((response) => response.text());
 
   // Insert a style tag into the wrapper view
